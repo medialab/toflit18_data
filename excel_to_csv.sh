e38087fdc6e7ef30672b530adfdc7f0c4380b140
Paul Girard <paul.girard@sciences-po.fr>
2015-02-04 17:16:52 +0100

    script to convert the many file formats and encodings from excel_sources into one clean sources directory

---
+++ excel_to_csv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -d ./sources ];
+then rm -rf ./sources;
+else mkdir sources;
+fi
+
+find  ./excel_sources -type f | while read f; do
+	dr=`echo $f | sed 's|./excel_sources/\(.*\)/[^/]\+.\w*$|\1|'`
+	filename=$(basename "$f")
+	extension="${filename##*.}"
+	echo $f
+	mkdir -p "./sources/$dr"
+	case $extension in  
+		xls | xlsx) unoconv -f csv -o "./sources/$dr/" "$f";; 
+		csv | txt) iconv -f MAC -t UTF8 -o "./sources/$dr/$filename" "$f";;
+		docx | doc) unoconv -f txt -o "./sources/$dr/" "$f";;
+	esac
+done;
+
+
