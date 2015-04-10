98a4a48f8ed53393d18721b50549f81f67b2828b
Paul Girard <paul.girard@sciences-po.fr>
2015-04-10 18:29:54 +0200

    first version

---
+++ scripts/aggregate_sources_in_bdd_centrale.py
@@ -0,0 +1,32 @@
+import csvkit
+import os
+
+output_filename="agg_to_tableau_generale.csv"
+directory="../sources"
+black_list=["Divers/AN/F_12_1835"]
+
+sources_aggregation=[]
+ordered_headers=["numrodeligne","dataentryby","source","SourcePath","SourceType","year","exportsimports","direction","bureaux","sheet","marchandises","pays","value","quantit","origine","total","quantity_unit","leurvaleursubtotal_1","leurvaleursubtotal_2","leurvaleursubtotal_3","prix_unitaire","probleme","remarks"]
+headers=[]
+
+for (dirpath,dirnames,filenames) in os.walk(directory):
+	if not sum(dirpath == os.path.join(directory,b) for b in black_list) :
+		for csv_file_name in filenames :
+			ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
+			if ext == "csv":
+				print "%s in %s"%(csv_file_name,dirpath)
+				with open(os.path.join(dirpath,csv_file_name),"r") as source_file:
+					r=csvkit.DictReader(source_file)
+					headers+=r.fieldnames
+					sources_aggregation+=list(r)
+sources_aggregation = sorted(sources_aggregation, key=lambda e:(e["SourceType"],e["year"],e["direction"] if "direction" in e else "",e["exportsimports"],e["numrodeligne"],e["marchandises"],e["pays"] if "pays" in e else ""))
+headers=set(headers)
+headers=[h for h in  headers if h not in ordered_headers]
+headers=ordered_headers+headers
+with open(output_filename,"w") as output_file:
+	agg_csv=csvkit.DictWriter(output_file,headers)
+	agg_csv.writeheader()
+	agg_csv.writerows(sources_aggregation)
+
+#csvsort  -c SourceType,year,direction,exportsimports,numrodeligne,marchandises,pays "$f" > last_ordered.csv
+			
