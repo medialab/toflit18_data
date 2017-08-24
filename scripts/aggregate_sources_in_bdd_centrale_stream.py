9bf8d1f3439345debe0ce2ed54818ab7c088c403
Yomguithereal <guillaumeplique@gmail.com>
2017-08-24 14:45:37 +0200

    Fixing all the Rennes Local files

--- scripts/aggregate_sources_in_bdd_centrale_stream.py
+++ scripts/aggregate_sources_in_bdd_centrale_stream.py
@@ -28,6 +28,67 @@ def clean(s):
     #s = re.sub(OEWRONG, u"œ", s)
     return s
 
+empty_value = lambda v : v=="0" or v=="." or v=="" or v=="?"
+
+def clean_float_string(f):
+    f=re.sub(r"[,，、،﹐﹑]",".",f)
+    f=re.sub(r"[\s ]","",f)
+    return f
+
+def add_fields_to_line(d):
+
+    d.setdefault("source", "")
+    d.setdefault("value", "")
+    d.setdefault("prix_unitaire", "")
+    d.setdefault("quantit", "")
+
+    if (
+        (not d["source"]=="") and
+        (not(d["value"]=="0" and d["quantit"]=="" and d["prix_unitaire"]=="")) and
+        (not(empty_value(d["value"]) and empty_value(d["quantit"]) and empty_value(d["prix_unitaire"])))
+    ):
+
+        d["value_as_reported"] = d["value"]
+        # false 0 to null
+        if d["value"]=="0" and not empty_value(d["quantit"]):
+            # nb_value_set_null+=1
+            d["value"]=""
+
+        #À CHANGER : IL FAUT DONNER LA PRIORITÉ AUX VALEURS CALCULÉES % AUX VALEURS
+        # Was the d["value"] computed expost based on unit price and quantities ? 0 no 1 yes
+        if not empty_value(d["prix_unitaire"]) and not empty_value(d["quantit"]):
+            d["computed_value"]=1
+            q=clean_float_string(d["quantit"])
+            pu=clean_float_string(d["prix_unitaire"])
+            try :
+                d["value"] = float(q)*float(pu)
+            except :
+                print "can't parse to float q: '%s' pu:'%s' "%(q,pu)
+                d["value"]=""
+            # nb_computed_value+=1
+        else :
+            d["computed_value"]=0
+
+        # Was the unit price computed expost based on and quantities and value ? 0 no 1 yes
+        if empty_value(d["prix_unitaire"]) and not empty_value(d["value"]) and not empty_value(d["quantit"]):
+            d["replace_computed_up"]=1
+            q=clean_float_string(d["quantit"])
+            v=clean_float_string(d["value"])
+            try:
+                d["prix_unitaire"] = float(v)/float(q)
+            except:
+                print "can't parse to float q: '%s' v:'%s' "%(q,v)
+                d["prix_unitaire"]=""
+
+            # nb_computed_value+=1
+        else :
+            d["replace_computed_up"]=0
+
+        # transform "." and "?" into ""
+        for field in ["value","quantit","prix_unitaire"]:
+            if d[field] in [".","?"]:
+                d[field]=""
+
 output_filename="../base/bdd_centrale.csv"
 directory="../sources"
 black_list=["Divers/AN/F_12_1835"]
@@ -51,6 +112,10 @@ headers=set(headers)
 headers=[h for h in  headers if h not in ordered_headers]
 headers=ordered_headers+headers
 
+for extra_header in ["value_as_reported","computed_value","replace_computed_up"]:
+    if extra_header not in headers:
+        headers+=[extra_header]
+
 # Then we actually read and write the lines
 with open(output_filename,"w") as output_file:
     writer = csvkit.DictWriter(output_file, headers, encoding="utf-8")
@@ -64,96 +129,16 @@ with open(output_filename,"w") as output_file:
                     print "%s in %s"%(csv_file_name,dirpath)
                     with open(os.path.join(dirpath,csv_file_name),"r") as source_file:
                         r=csvkit.DictReader(source_file)
-                        headers+=r.fieldnames
 
                         for line in r:
                             for k in line:
                                 line[k] = clean(line[k])
+
+                            # add_fields_to_line(line)
+
                             writer.writerow(line)
 
 
 #csvsort  -c SourceType,year,direction,exportsimports,numrodeligne,marchandises,pays "$f" > last_ordered.csv
 
 #taking care of 0/missing in "values" and in "prix_unitaire"
-
-def clean_float_string(f):
-    f=re.sub(r"[,，、،﹐﹑]",".",f)
-    f=re.sub(r"[\s ]","",f)
-    return f
-
-print 'Finished first phase.'
-
-with open(output_filename,"r") as output_file:
-    lines = csvkit.DictReader(output_file)
-    lines.next()
-    headers=lines.fieldnames
-
-# nb_lines_before = len(lines)
-
-empty_value = lambda v : v=="0" or v=="." or v=="" or v=="?"
-
-lines = (d for d in lines if not d["source"]=="" )
-lines = (d for d in lines if not(d["value"]=="0" and d["quantit"]=="" and d["prix_unitaire"]=="")) #Dans tous les cas regardés le 31 mai 2016, ce sont des "vrais" 0
-lines = (d for d in lines if not(empty_value(d["value"]) and empty_value(d["quantit"]) and empty_value(d["prix_unitaire"])))
-# print "removed %s empty or missing value lines"%(nb_lines_before - len(lines))
-
-nb_value_set_null=0
-nb_computed_value=0
-nb_computed_value=0
-for d in lines :
-    d["value_as_reported"] = d["value"]
-    # false 0 to null
-    if d["value"]=="0" and not empty_value(d["quantit"]):
-        nb_value_set_null+=1
-        d["value"]=""
-
-
-    #À CHANGER : IL FAUT DONNER LA PRIORITÉ AUX VALEURS CALCULÉES % AUX VALEURS
-    # Was the d["value"] computed expost based on unit price and quantities ? 0 no 1 yes
-    if not empty_value(d["prix_unitaire"]) and not empty_value(d["quantit"]):
-        d["computed_value"]=1
-        q=clean_float_string(d["quantit"])
-        pu=clean_float_string(d["prix_unitaire"])
-        try :
-            d["value"] = float(q)*float(pu)
-        except :
-            print "can't parse to float q: '%s' pu:'%s' "%(q,pu)
-            d["value"]=""
-        nb_computed_value+=1
-    else :
-        d["computed_value"]=0
-
-    # Was the unit price computed expost based on and quantities and value ? 0 no 1 yes
-    if empty_value(d["prix_unitaire"]) and not empty_value(d["value"]) and not empty_value(d["quantit"]):
-        d["replace_computed_up"]=1
-        q=clean_float_string(d["quantit"])
-        v=clean_float_string(d["value"])
-        try:
-            d["prix_unitaire"] = float(v)/float(q)
-        except:
-            print "can't parse to float q: '%s' v:'%s' "%(q,v)
-            d["prix_unitaire"]=""
-
-        nb_computed_value+=1
-    else :
-        d["replace_computed_up"]=0
-
-    # transform "." and "?" into ""
-    for field in ["value","quantit","prix_unitaire"]:
-        if d[field] in [".","?"]:
-            d[field]=""
-
-#18000
-print "removed %s false 0 values"%nb_value_set_null
-#10000
-print "computed %s values from quantit*prix_unitaire "%nb_computed_value
-#82000
-print "computed %s prix_unitaire from value/quantit"%nb_computed_value
-#83000 // 200
-for extra_header in ["value_as_reported","computed_value","replace_computed_up"]:
-    if extra_header not in headers:
-        headers+=[extra_header]
-with open(output_filename,"w") as output_file:
-    writefile=csvkit.DictWriter(output_file,fieldnames=headers)
-    writefile.writeheader()
-    writefile.writerows(lines)
