c1414319aa4f50ed031722b7cc213bed7f14d53f
Yomguithereal <guillaumeplique@gmail.com>
2017-08-31 15:08:02 +0200

    Glossaire generation script

---
+++ scripts/generate_glossaire_json.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv('../traitements_marchandises/01RepertoireMarchandises.csv')
+df = df[df['Inconnu'] != 1]
+df = df.rename(columns={'marchandises': 'name'})
+df = df.filter(items=['name', 'definition'])
+
+json = df.to_json(orient='records')
+
+print json
