534065f489671d37cfc25aba2c9202f24b4905f9
Yomguithereal <guillaumeplique@gmail.com>
2017-09-05 14:46:05 +0200

    Dropping duplicates in glossary entries

--- scripts/generate_glossaire_json.py
+++ scripts/generate_glossaire_json.py
@@ -4,6 +4,7 @@ df = pd.read_csv('../traitements_marchandises/01RepertoireMarchandises.csv')
 df = df[df['Inconnu'] != 1]
 df = df.rename(columns={'marchandises': 'name'})
 df = df.filter(items=['name', 'definition'])
+df = df.drop_duplicates(subset='name')
 
 json = df.to_json(orient='records')
 
