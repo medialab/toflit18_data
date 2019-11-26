435414d30092ca6c5bb1c891b7c5befe503c7d5f
Paul Girard <paul.girard@sciencespo.fr>
2019-11-26 17:39:00 +0100

    refacto aggregate script

--- scripts/aggregate_sources_in_bdd_centrale.py
+++ scripts/aggregate_sources_in_bdd_centrale.py
@@ -34,7 +34,7 @@ def clean_float_string(f):
     f=re.sub(r"[\s ]","",f)
     return f
 
-def add_fields_to_line(d):
+def add_calculated_fields_to_line(d):
 
     d.setdefault("source", "")
     d.setdefault("value", "")
@@ -88,62 +88,62 @@ def add_fields_to_line(d):
         if d[field] in [".","?"]:
             d[field]=""
 
-output_filename="../base/bdd_centrale.csv"
-directory="../sources"
-black_list=["Divers/AN/F_12_1835", "Out"]
+def aggregate_sources_in_bdd_centrale(with_calculated_values = False):
+    output_filename="../base/bdd_centrale.csv"
+    directory="../sources"
+    black_list=["Divers/AN/F_12_1835", "Out"]
 
-sources_aggregation=[]
-ordered_headers=["numrodeligne","dataentryby","source","sourcepath","sourcetype","year","exportsimports","direction","bureaux","sheet","marchandises","pays","value","quantit","origine","total","quantity_unit","leurvaleursubtotal_1","leurvaleursubtotal_2","leurvaleursubtotal_3","prix_unitaire","probleme","remarks","unverified"]
-headers=[]
+    sources_aggregation=[]
+    ordered_headers=["numrodeligne","dataentryby","source","sourcepath","sourcetype","year","exportsimports","direction","bureaux","sheet","marchandises","pays","value","quantit","origine","total","quantity_unit","leurvaleursubtotal_1","leurvaleursubtotal_2","leurvaleursubtotal_3","prix_unitaire","probleme","remarks","unverified"]
+    headers=[]
 
-# First we need to read the headers
-for (dirpath,dirnames,filenames) in os.walk(directory):
-    if not sum(dirpath == os.path.join(directory,b) for b in black_list) :
-        for csv_file_name in filenames :
-            ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
-            if ext == "csv":
-                # print "%s in %s"%(csv_file_name,dirpath)
-                with open(os.path.join(dirpath,csv_file_name),"r",  encoding="utf-8") as source_file:
-                    r=DictReader(source_file)
-                    headers+=r.fieldnames
-
-headers=set(headers)
-headers=[h for h in  headers if h not in ordered_headers]
-headers=ordered_headers+headers
-
-for extra_header in ["value_as_reported","computed_value","replace_computed_up"]:
-    if extra_header not in headers:
-        headers+=[extra_header]
-
-# Then we actually read and write the lines
-with open(output_filename,"w", encoding="utf-8") as output_file:
-    writer = DictWriter(output_file, headers)
-    writer.writeheader()
-
+    # First we need to read the headers
     for (dirpath,dirnames,filenames) in os.walk(directory):
         if not sum(dirpath == os.path.join(directory,b) for b in black_list) :
             for csv_file_name in filenames :
                 ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
                 if ext == "csv":
-                    print("%s in %s"%(csv_file_name,dirpath))
-
-                    filepath = os.path.join(dirpath, csv_file_name)
-
-                    with open(filepath,"r", encoding="utf-8") as source_file:
+                    # print "%s in %s"%(csv_file_name,dirpath)
+                    with open(os.path.join(dirpath,csv_file_name),"r",  encoding="utf-8") as source_file:
                         r=DictReader(source_file)
+                        headers+=r.fieldnames
 
-                        for line in r:
-                            for k in line:
-                                line[k] = clean(line[k])
+    headers=set(headers)
+    headers=[h for h in  headers if h not in ordered_headers]
+    headers=ordered_headers+headers
 
-                            # if not filepath.decode('utf-8').endswith(line['sourcepath']):
-                            #     print 'WARNING: incorrect sourcepath!'
-							#     raise Exception('incorrect sourcepath')
-                            if (line['sourcetype'] != "" or line['sourcepath'] != "") and line['sourcetype'] !="Out":
-                               add_fields_to_line(line)
-                               writer.writerow(line)
+    for extra_header in ["value_as_reported","computed_value","replace_computed_up"]:
+        if extra_header not in headers:
+            headers+=[extra_header]
 
+    # Then we actually read and write the lines
+    with open(output_filename,"w", encoding="utf-8") as output_file:
+        writer = DictWriter(output_file, headers)
+        writer.writeheader()
 
-#csvsort  -c SourceType,year,direction,exportsimports,numrodeligne,marchandises,pays "$f" > last_ordered.csv
+        for (dirpath,dirnames,filenames) in os.walk(directory):
+            if not sum(dirpath == os.path.join(directory,b) for b in black_list) :
+                for csv_file_name in filenames :
+                    ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
+                    if ext == "csv":
+                        print("%s in %s"%(csv_file_name,dirpath))
 
-#taking care of 0/missing in "values" and in "prix_unitaire"
+                        filepath = os.path.join(dirpath, csv_file_name)
+
+                        with open(filepath,"r", encoding="utf-8") as source_file:
+                            r=DictReader(source_file)
+
+                            for line in r:
+                                for k in line:
+                                    line[k] = clean(line[k])
+
+                                # if not filepath.decode('utf-8').endswith(line['sourcepath']):
+                                #     print 'WARNING: incorrect sourcepath!'
+                                #     raise Exception('incorrect sourcepath')
+                                if (line['sourcetype'] != "" or line['sourcepath'] != "") and line['sourcetype'] !="Out":
+                                    if with_calculated_values:
+                                        add_calculated_fields_to_line(line)
+                                    writer.writerow(line)
+
+if __name__ == "__main__":
+    aggregate_sources_in_bdd_centrale(True)
