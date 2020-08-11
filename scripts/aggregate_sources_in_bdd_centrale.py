96dc508efddea0ddcffac399568dc9f11c3b5d94
Paul Girard <paul@ouestware.com>
2020-08-11 10:33:52 +0200

    aggregation/split scripts compatible with datapackage format

--- scripts/aggregate_sources_in_bdd_centrale.py
+++ scripts/aggregate_sources_in_bdd_centrale.py
@@ -3,7 +3,7 @@
 import os
 import re
 from csv import DictReader, DictWriter
-import datapackage
+import json
 
 DOUBLE_QUOTES = r'[«»„‟“”"]'
 SIMPLE_QUOTES = r"[’‘`‛']"
@@ -39,27 +39,27 @@ def add_calculated_fields_to_line(d):
 
     d.setdefault("source", "")
     d.setdefault("value", "")
-    d.setdefault("prix_unitaire", "")
-    d.setdefault("quantit", "")
+    d.setdefault("value_unit", "")
+    d.setdefault("quantity", "")
 
     if (
         (not d["source"]=="") and
-        (not(d["value"]=="0" and d["quantit"]=="" and d["prix_unitaire"]=="")) and
-        (not(empty_value(d["value"]) and empty_value(d["quantit"]) and empty_value(d["prix_unitaire"])))
+        (not(d["value"]=="0" and d["quantity"]=="" and d["value_unit"]=="")) and
+        (not(empty_value(d["value"]) and empty_value(d["quantity"]) and empty_value(d["value_unit"])))
     ):
 
         d["value_as_reported"] = d["value"]
         # false 0 to null
-        if d["value"]=="0" and not empty_value(d["quantit"]):
+        if d["value"]=="0" and not empty_value(d["quantity"]):
             # nb_value_set_null+=1
             d["value"]=""
 
         #À CHANGER : IL FAUT DONNER LA PRIORITÉ AUX VALEURS CALCULÉES % AUX VALEURS
         # Was the d["value"] computed expost based on unit price and quantities ? 0 no 1 yes
-        if not empty_value(d["prix_unitaire"]) and not empty_value(d["quantit"]):
+        if not empty_value(d["value_unit"]) and not empty_value(d["quantity"]):
             d["computed_value"]=1
-            q=clean_float_string(d["quantit"])
-            pu=clean_float_string(d["prix_unitaire"])
+            q=clean_float_string(d["quantity"])
+            pu=clean_float_string(d["value_unit"])
             try :
                 d["value"] = float(q)*float(pu)
             except :
@@ -70,82 +70,85 @@ def add_calculated_fields_to_line(d):
             d["computed_value"]=0
 
         # Was the unit price computed expost based on and quantities and value ? 0 no 1 yes
-        if empty_value(d["prix_unitaire"]) and not empty_value(d["value"]) and not empty_value(d["quantit"]):
+        if empty_value(d["value_unit"]) and not empty_value(d["value"]) and not empty_value(d["quantity"]):
             d["replace_computed_up"]=1
-            q=clean_float_string(d["quantit"])
+            q=clean_float_string(d["quantity"])
             v=clean_float_string(d["value"])
             try:
-                d["prix_unitaire"] = float(v)/float(q)
+                d["value_unit"] = float(v)/float(q)
             except:
                 print("can't parse to float q: '%s' v:'%s' "%(q,v))
-                d["prix_unitaire"]=""
+                d["value_unit"]=""
 
             # nb_computed_value+=1
         else :
             d["replace_computed_up"]=0
 
     # transform "." and "?" into ""
-    for field in ["value","quantit","prix_unitaire"]:
+    for field in ["value","quantity","value_unit"]:
         if d[field] in [".","?"]:
             d[field]=""
 
 def aggregate_sources_in_bdd_centrale(with_calculated_values = False):
-    output_filename="../base/bdd_centrale.csv"
-    directory="../sources"
-    black_list=[]#["Divers/AN/F_12_1835", "Out"]
+    output_filename = "../base/bdd_centrale.csv"
+    directory = "../sources"
+    black_list = []
 
-    sources_aggregation=[]
-    ordered_headers=["numrodeligne","dataentryby","source","sourcepath","sourcetype","year","exportsimports","direction","bureaux","sheet","marchandises","pays","value","quantit","origine","total","quantity_unit","leurvaleursubtotal_1","leurvaleursubtotal_2","leurvaleursubtotal_3","prix_unitaire","probleme","remarks","unverified"]
-    headers=[]
+    sources_aggregation = []
+    with open('../csv_sources_schema.json', 'r', encoding='utf8') as f:
+        toflit18_flow_schema = json.load(f)
+        ordered_headers = [f['name'] for f in toflit18_flow_schema['fields']]
+    # headers = []
 
-    # First we need to read the headers
-    for (dirpath,dirnames,filenames) in os.walk(directory):
-        if not sum(dirpath == os.path.join(directory,b) for b in black_list) :
-            for csv_file_name in filenames :
-                ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
-                if ext == "csv":
-                    # print "%s in %s"%(csv_file_name,dirpath)
-                    with open(os.path.join(dirpath,csv_file_name),"r",  encoding="utf-8") as source_file:
-                        r=DictReader(source_file)
-                        headers+=r.fieldnames
+    # # First we need to read the headers
+    # for (dirpath, dirnames, filenames) in os.walk(directory):
+    #     if not sum(dirpath == os.path.join(directory, b) for b in black_list):
+    #         for csv_file_name in filenames:
+    #             ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
+    #             if ext == "csv":
+    #                 # print "%s in %s"%(csv_file_name,dirpath)
+    #                 with open(os.path.join(dirpath,csv_file_name), "r", encoding="utf-8") as source_file:
+    #                     r = DictReader(source_file)
+    #                     headers += r.fieldnames
 
-    headers=set(headers)
-    headers=[h for h in  headers if h not in ordered_headers]
-    headers=ordered_headers+headers
-
+    # headers = set(headers)
+    # headers = [h for h in  headers if h not in ordered_headers]
+    headers = [h for h in ordered_headers] #+headers
     if with_calculated_values:
-        for extra_header in ["value_as_reported","computed_value","replace_computed_up"]:
+        for extra_header in ["value_as_reported", "computed_value", "replace_computed_up"]:
             if extra_header not in headers:
                 headers+=[extra_header]
 
     # Then we actually read and write the lines
-    with open(output_filename,"w", encoding="utf-8") as output_file:
+    with open(output_filename, "w", encoding="utf-8") as output_file:
         writer = DictWriter(output_file, headers)
         writer.writeheader()
 
-        for (dirpath,dirnames,filenames) in os.walk(directory):
-            if not sum(dirpath == os.path.join(directory,b) for b in black_list) :
-                for csv_file_name in filenames :
+        for (dirpath, dirnames, filenames) in os.walk(directory):
+            if not sum(dirpath == os.path.join(directory, b) for b in black_list):
+                for csv_file_name in filenames:
                     ext = csv_file_name.split(".")[-1] if "." in csv_file_name else None
                     if ext == "csv":
-                        print("%s in %s"%(csv_file_name,dirpath))
+                        print("%s in %s"%(csv_file_name, dirpath))
 
                         filepath = os.path.join(dirpath, csv_file_name)
 
-                        with open(filepath,"r", encoding="utf-8") as source_file:
-                            r=DictReader(source_file)
-
+                        with open(filepath, "r", encoding="utf-8") as source_file:
+                            r = DictReader(source_file)
+                            if r.fieldnames != ordered_headers:
+                                print("%s file does not respect the format"%filepath)
+                                print(r.fieldnames)
+                                exit(1)
                             for line in r:
                                 for k in line:
                                     line[k] = clean(line[k])
 
-                                # if not filepath.decode('utf-8').endswith(line['sourcepath']):
-                                #     print 'WARNING: incorrect sourcepath!'
-                                #     raise Exception('incorrect sourcepath')
-                                if (line['sourcetype'] != "" or line['sourcepath'] != "")  : # and line['sourcetype'] !="Out"
-                                    if with_calculated_values:
-                                        add_calculated_fields_to_line(line)
-                                    writer.writerow(line)
+                                if filepath.replace('../','') != line['filepath']:
+                                    print('ERROR: incorrect filepath does not match\n%s\n%s'%(filepath,line['filepath']))
+                                    raise Exception('incorrect filepath for line \n%s'%line)
+                                if with_calculated_values:
+                                    add_calculated_fields_to_line(line)
+                                writer.writerow(line)
 
 if __name__ == "__main__":
     aggregate_sources_in_bdd_centrale(False)
---
+++ scripts/split_bdd_centrale_in_sources.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+import itertools
+import os
+from csv import DictReader, writer, DictWriter
+import re
+import json
+from unidecode import unidecode
+import collections
+import shutil
+import csv
+
+WRITE = True
+VERBOSE = True
+REMOVE_COLUMN = False
+SOURCES_ROOT_DIR = '..'
+
+def cast_numrodeligne(value):
+    if not len(value):
+        return 0
+
+    try:
+        return int(value)
+    except:
+        return 0
+
+with open("../base/bdd_centrale.csv", encoding='utf-8') as bdd_centrale:
+
+    reader = DictReader(bdd_centrale)
+
+    # check bdd_centrale format
+    with open('../csv_sources_schema.json', 'r', encoding='utf8') as f:
+        toflit18_flow_schema = json.load(f)
+        flows_headers = [f['name'] for f in toflit18_flow_schema['fields']]
+        flows_fields = set(flows_headers)
+        bdd_fields = set(reader.fieldnames)
+        if bdd_fields != flows_fields:
+            missing_fields = flows_fields - bdd_fields
+            extra_fields = bdd_fields - flows_fields
+            if len(missing_fields) > 0:
+                print("columns are missing in bdd_centrale.csv: %s" % missing_fields)
+            if len(extra_fields) > 0:
+                print("Unexpected columns in bdd_centrale.csv: %s" % extra_fields)
+            raise Exception('bdd_centrale.csv does not respect flows format')
+
+    data = list(reader)
+
+    # line sort order
+    # source_type / year / tax_department / export_import / numéro de ligne / product / partner
+    lines_key_sort = lambda k: (k.get('source_type',''), k.get('year', ''), k.get('tax_department', ''), k.get('export_import', ''), cast_numrodeligne(k.get('line_number', 0)), k.get('product', ''), k.get('partner',''))
+
+    sort_bdd_centrale = lambda r: (r['source_type'], r['filepath'])
+    data = sorted(data, key=sort_bdd_centrale)
+    # list existing files and count lines
+    existing_files = {}
+    for (dirpath, dirnames, filenames) in os.walk(os.path.join(SOURCES_ROOT_DIR, 'sources')):
+        for csv_file_name in filenames:
+            filepath = os.path.join(dirpath, csv_file_name)
+            with open(filepath) as f:
+                existing_files[filepath] = sum((1 for _ in f)) - 1
+    if WRITE:
+        shutil.rmtree(os.path.join(SOURCES_ROOT_DIR, 'sources'))
+    datapackage_resource_path = []
+    with open('desagregate_bdd_centrale_in_sources.csv', 'w', encoding='utf8') as report_f:
+        report = csv.DictWriter(report_f, fieldnames=["filepath", "nb_line", "diff_nb_line", "sources", "source_types"])
+        report.writeheader()
+        for (source_type, filename), data in itertools.groupby(data, key=sort_bdd_centrale):
+            if filename == "":
+                print("sourcefilename IS empty ARRRG")
+                for l in g:
+                    print(l)
+            # let's sort
+            source_data = list(data)
+            real_path = os.path.join(SOURCES_ROOT_DIR, filename)
+            nb_lines_bdd_centrale = len(source_data)
+            nb_lines_existing_file = existing_files[real_path] if real_path in existing_files else 0
+            source_data = sorted(source_data, key=lines_key_sort)
+
+            print("%s:%s>%s" % (filename, nb_lines_existing_file, nb_lines_bdd_centrale))
+
+            
+            if WRITE:
+                path = os.path.join(SOURCES_ROOT_DIR, 'sources', source_type)
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                with open(real_path, 'w', encoding='utf8') as output:
+                    writer = DictWriter(output, fieldnames=flows_headers)
+                    writer.writeheader()
+                    writer.writerows(source_data)
+                datapackage_resource_path.append(filename)
+
+            report.writerow({
+                'filepath': filename,
+                'nb_line': nb_lines_bdd_centrale,
+                'diff_nb_line': nb_lines_bdd_centrale - nb_lines_existing_file,
+                'sources': ";".join(sorted(set(d['source'] for d in source_data))),
+                'source_types': ";".join(sorted(set(d['source_type'] for d in source_data)))
+            })
+
+    datapackage_resource = collections.OrderedDict([     
+        ("name", "flows"),
+        ("mediatype", "text/csv"),
+        ("format", "csv"),
+        ("encoding", "UTF8"),
+        ("profile", "tabular-data-resource"), 
+        ("schema", "csv_sources_schema.json"),
+        ("sources", [{"title":"French Bureau de la balance du commerce statistics see http://toflit18.medialab.sciences-po.fr/#/exploration/sources for more details"}]),
+        ("path", datapackage_resource_path),
+    ])
+
+
+# update datapackage
+datapackage = {}
+with open("../datapackage.json", "r", encoding="utf8") as datapackage_file:
+    datapackage = json.load(datapackage_file, object_pairs_hook=collections.OrderedDict)
+    # replace flow resource
+    datapackage['resources'][-1] = datapackage_resource
+with open("../datapackage.json", "w", encoding='utf8') as new_datapackage_file:
+    json.dump(datapackage, new_datapackage_file, indent=2, ensure_ascii=False)
