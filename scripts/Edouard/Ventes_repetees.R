d43b467235e50fed3fd4ff68b6267f7ad0d1c04c
edouardpignede <edouard.pignede@student.ecp.fr>
2021-02-25 19:42:07 +0100

    Création de l'indice par la méthode chaînée

--- scripts/Edouard/Ventes_repetees.R
+++ scripts/Edouard/Ventes_repetees.R
@@ -4,8 +4,8 @@ library(stringr)
 library(readr)
 library(pracma)
 
-library(IndexNumR)
 library(hpiR)
+
 
 
 ### Calcul des indices de prix : 1er exemple
@@ -16,193 +16,31 @@ setwd("C:/Users/pignede/Documents/GitHub/toflit18_data")
 
 rm(list = ls())
 
-### On fixe la ville que l'on veut étudier
-ville = "Nantes"
+### On charge la fonction de filtrage
+source("./scripts/Edouard/Filtrage.R")
+
+### On filtre la bdd courante :
 
 
-### On importe la base de données courante
-bdd_courante <- read.csv("./base/bdd courante.csv", encoding = "UTF-8")
-
-Data <- bdd_courante %>%
-  select(c("year", "customs_region", "export_import", "partner_orthographic",
-           "product_simplification", "quantity_unit_metric", "quantities_metric", "unit_price_metric",
-           "best_guess_national_prodxpart", "best_guess_national_partner", 
-           "best_guess_national_product", "best_guess_national_region",
-           "best_guess_region_prodxpart")) %>%
-  mutate(Date = as.Date(as.character(year), format = "%Y")) %>%
-  ### On selectionne uniquement les produits rangés par régions
-  filter(best_guess_region_prodxpart == 1) %>%
-  ### On selectionne uniquement le port de Marseille
-  filter(customs_region == ville) %>%
-  mutate_if(is.character, as.factor) %>%
-  ### Création ID product_simplification et ID transaction
-  mutate(id_prod_simp = as.numeric(product_simplification),
-         id_trans = row_number()) %>%
-  ### On enlève les transactions sans prix et les transactions avec un prix nul
-  mutate(unit_price_metric = na_if(unit_price_metric, 0)) %>%
-  drop_na(unit_price_metric) %>%
-  ### On crée une dummy variable best_unit_metric qui pour chaque transaction vaut 1 
-  ### si la transacton est dans l'unité métrique la plus utilisée pour le produit
-  group_by(product_simplification) %>%
-  mutate(best_unit_metric = names(which.max(table(quantity_unit_metric)))) %>%
-  ungroup() %>%
-  as.data.frame() %>%
-  mutate(best_unit_metric = best_unit_metric == quantity_unit_metric ) %>%
-  ### Création d'une variable qui récupère pour chaque produits le nombre de transactions
-  ### dans la meilleure unité métrique
-  group_by(product_simplification) %>%
-  mutate(trans_number = sum(best_unit_metric)) %>%
-  ungroup() %>%
-  as.data.frame()
+Data_filter <- Data_filtrage(Ville = "Nantes",  ### Choix du port d'étude
+                             Outliers = F, ### conservation des outliers 
+                             Outliers_coef = 1.5, ### Quel niveau d'écart inter Q garde-t-on
+                             Trans_number = 0, ### On retire les produits vendus moins de Trans_number fois
+                             Exports_imports = "Imports", ### On conserve les Importations ou les Exportations
+                             Prod_problems = T) ### Conserve-t-on les produits avec des différences de prix très importants
 
 
-
-
-
-
-# ### On observe l'évolution des prix unitaires de tous les produits importés
-# ### plus de xxxx fois sur la période
-# for (prod in levels(Data$product_simplification)) {
-#   
-#   if (sum(Data$product_simplification == prod
-#           & Data$export_import == "Imports"
-#           & Data$best_unit_metric == T) > 20) {
-#     
-#     Data = subset(Data,  product_simplification == prod 
-#                   & export_import == "Imports"
-#                   & best_unit_metric == T)
-#     print(prod)
-#     print(Data[, c("quantity_unit_metric", "unit_price_metric")])
-#     plot(Data$year, Data$unit_price_metric, main = prod)
-#   }
-# }
-
-
-
-### Détéection valeurs aberrantes
-outliers_trans <- c()
-for (prod in levels(Data$product_simplification)) {
-  
-  Data_imports = subset(Data,  product_simplification == prod 
-                & export_import == "Imports"
-                & best_unit_metric == T)
-  
-  Data_exports = subset(Data,  product_simplification == prod 
-                        & export_import == "Exports"
-                        & best_unit_metric == T)
-  
-  
-  outliers_trans <- c(outliers_trans,
-                      Data_imports$id_trans[which(Data_imports$unit_price_metric %in% 
-                                            boxplot.stats(Data_imports$unit_price_metric, coef = 1.5)$out)])
-  
-  outliers_trans <- c(outliers_trans,
-                      Data_exports$id_trans[which(Data_exports$unit_price_metric %in% 
-                                                    boxplot.stats(Data_exports$unit_price_metric, coef = 1.5)$out)])
-}
-
-Data <- Data %>%
-  mutate(outliers = id_trans %in% outliers_trans)
-
-
-
-
-
-
-
-
-
-# ### Détection des valeurs aberrantes : Méthode robuste sur les séries temporelles 
-# for (prod in levels(Data$product_simplification)) {
-#   
-#   Data_imports = subset(Data,  product_simplification == prod 
-#                         & export_import == "Imports"
-#                         & best_unit_metric == T)
-#   
-#   locate.outliers(resid, pars, cval = 3.5, types = c("AO", "LS", "TC"), 
-#                   delta = 0.7, n.start = 50)
-#   
-#   
-# }
-
-
-
-
-
-# ### On observe l'évolution des prix unitaires de tous les produits importés
-# ### plus de xxxx fois sur la période
-# for (prod in levels(Data$product_simplification)) {
-#   
-#   if (sum(Data$product_simplification == prod
-#           & Data$export_import == "Imports"
-#           & Data$best_unit_metric == T) > 20) {
-#     
-#     Data = subset(Data,  product_simplification == prod 
-#                   & export_import == "Imports"
-#                   & best_unit_metric == T
-#                   & outliers == F)
-#     print(prod)
-#     print(Data[, c("quantity_unit_metric", "unit_price_metric")])
-#     plot(Data$year, Data$unit_price_metric, main = prod)
-#   }
-# }
-
-
-
-
-
-
-
-
-### Calcul de l'indice de prix par le méthode des ventes répétées
-### https://cran.r-project.org/web/packages/hpiR/vignettes/introduction.html
-
-### filtrage de la base de données 
-
-Data_filter <- Data %>%
-  filter(best_unit_metric == T
-         & export_import == "Imports"
-         & outliers == F) %>%
-  
-  ### On compte le nombre de transations sur la base de données filtrées 
-  group_by(product_simplification) %>%
-  mutate(trans_number = sum(best_unit_metric)) %>%
-  ungroup() %>%
-  as.data.frame() %>%
-  
-  ### On filtre le nombre de transactions utilisées dans la méthode
-  filter(trans_number > 20)
-
-
-
-### ON selectionne les produits pour lesquels il existe une différence trop importante de prix :
-### multiplication des prix par 10 entre le plus petit et le plus grand quartile 
-prod_problems <- c()
-for (prod in levels(Data$product_simplification)) {
-  
-  Data_prod <- subset(Data_filter, product_simplification == prod)
-  
-  if (dim(Data_prod)[1] > 0) {
-  
-  if(quantile(Data_prod$unit_price_metric, probs = 3/4)/quantile(Data_prod$unit_price_metric, probs = 1/4) > 10) {
-    prod_problems <- c(prod_problems, prod) 
-  }
-    }
-}
-
-
-Data_filter <- Data_filter %>%
-  filter(!product_simplification %in% prod_problems)
-  
+### Creation des colonnes de colonnes
+Data_period <- dateToPeriod(trans_df = Data_filter,
+                           date = 'Date',
+                           periodicity = 'yearly')
 
 
 ### Création de la base de données des transactions considérées
-Data_trans <- rtCreateTrans(trans_df = Data_filter,
+Data_trans <- rtCreateTrans(trans_df = Data_period,
                                 prop_id = "id_prod_simp",
                                 trans_id = "id_trans",
                                 price = "unit_price_metric",
-                                date = "Date",
-                                periodicity = "yearly",
                                 min_period_dist = 0,
                                 seq_only = T)
 
@@ -210,18 +48,69 @@ Data_trans <- rtCreateTrans(trans_df = Data_filter,
 ### Application du modèle
 rt_model <- hpiModel(model_type = "rt",
                      hpi_df = Data_trans,
-                     estimator = "base",
-                     log_dep = T)
+                     estimator = "weighted",
+                     log_dep = T,
+                     trim_model = T,
+                     mod_spec = NULL)
 
 ### Calacul de l'indice
 rt_index <- modelToIndex(rt_model)
+rt_index$numeric <- rt_index$period
+
+### Smooth index
+smooth_index <- smoothIndex(rt_index,
+                            order = 5,
+                            in_place = T)
 
 ### Affichage du résultat
-plot(rt_index$period, rt_index$value, type = "l")
+### Indice brut
+plot(rt_index)
+### Smooth index
+plot(smooth_index, smooth = T)
 
 
 
 
+
+index_vol <- calcVolatility(index = rt_index$value,
+                            window = 3)
+
+
+
+### Evaluation de l'indice
+rt_index <- rtIndex(trans_df = Data_trans,
+                    estimator = 'weighted',
+                    log_dep = TRUE,
+                    trim_model = TRUE,
+                    smooth = TRUE,
+                    smooth_order = 5)
+### Calcul de la volatilité
+index_vol <- calcVolatility(index = rt_index$index$value,
+                            window = 3)
+plot(index_vol)
+
+### Calcul de la précision
+rt_accuracy <- calcAccuracy(hpi_obj = rt_index,
+                            test_type = 'rt',
+                            test_method = 'kfold',
+                            k = 10,
+                            seed = 123,
+                            smooth = T)
+
+plot(rt_accuracy)
+
+
+rt_series <- createSeries(hpi_obj = rt_index,
+                          train_period = 24,
+                          max_period = 30)
+
+plot(rt_series)
+
+rt_series <- calcSeriesAccuracy(series_obj = rt_series,
+                                test_method = 'forecast',
+                                test_type = 'rt',
+                                smooth = TRUE,
+                                in_place = TRUE)
 
 
 
@@ -237,3 +126,6 @@ for (prod in levels(Data_filter$product_simplification)) {
     plot(Data_prod$year, Data_prod$unit_price_metric, main = prod)
     }
 }
+
+
+
