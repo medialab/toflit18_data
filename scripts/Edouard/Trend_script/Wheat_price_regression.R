20b844ca05f0b4b270c4e399af706322d5def824
edouardpignede <edouard.pignede@student.ecp.fr>
2021-04-28 18:00:00 +0200

    Test de cointégration des commodités avec les indices

--- scripts/Edouard/Trend_script/Wheat_price_regression.R
+++ scripts/Edouard/Trend_script/Wheat_price_regression.R
@@ -9,6 +9,19 @@ library(stringr)
 library(readr)
 library(pracma)
 
+library(openxlsx)
+
+library(urca)
+
+
+
+### A définir: emplacement du working directory
+setwd("C:/Users/pignede/Documents/GitHub/toflit18_data")
+### setwd("/Users/Edouard/Dropbox (IRD)/IRD/Missions/Marchandises_18eme")
+
+### Nettoyage de l'espace de travail
+rm(list = ls())
+
 
 ### Chargement des données de prix des commodités
 
@@ -19,33 +32,44 @@ Index_res <- read.csv2("./scripts/Edouard/Index_results.csv", row.names = NULL, 
 
 
 
-for (Ville_cons in c("Marseille", "Bordeaux", "Rennes")) {
+for (Ville_cons in c("Marseille", "Bordeaux", "Rennes", "La Rochelle")) {
 
   Index_res_ville <- Index_res %>%
     ### On conserve uniquement la baseline
-    filter(Ville == Ville_cons, Exports_imports == "Imports", Outliers == T & Outliers_coef == 10 & Trans_number == 0 & Prod_problems == F &
+    filter(Ville == Ville_cons, Exports_imports == "Exports", Outliers == T & Outliers_coef == 10 & Trans_number == 0 & Prod_problems == F &
              Product_select == F & Remove_double == T & Ponderation == T & Pond_log == F) %>%
     select(c("year", "Index_value"))
   
+  if(Ville_cons == "Nantes") {
+    Ville_cons_wheat = "Orléans"
+  } else if (Ville_cons == "La Rochelle") {
+    Ville_cons_wheat = "Bordeaux"
+  } else { Ville_cons_wheat = Ville_cons}
   
-  Index_and_commodity <- merge(Index_res_ville, Wheat_price[, c("Market", Ville_cons)], 
+  
+  Index_and_commodity <- merge(Index_res_ville, Wheat_price[, c("Market", Ville_cons_wheat)], 
                                by.x = "year", by.y = "Market", all.x = T)
   names(Index_and_commodity) <- c("year", "Index_value", "Wheat_price")
   
   
   print(Ville_cons)
-  reg <- lm(Index_value ~ Wheat_price, data = Index_and_commodity)
+  reg <- lm(log(Index_value) ~ log(Wheat_price), data = Index_and_commodity)
   print(summary(reg))
   
   
   plot(Index_value ~ year, data = Index_and_commodity, type = "o")
   plot(Wheat_price ~ year, data = Index_and_commodity, type = "o")
   
-  print(cor(Index_and_commodity, use = "complete.obs"))
+  ###print(cor(Index_and_commodity, use = "complete.obs"))
+  
+  
+  cointegration <- ca.jo(Index_and_commodity[, c("Index_value", "Wheat_price")])
+  print(summary(cointegration))
   
 
 }
 
 
-"Nantes" = "Chateau-Gontier"
+"Nantes" = "Orleans"
+"La Rochelle" = "Bordeaux"
 
