69cb75591226e0f8d49d1036f1e94d6b7be2af07
edouardpignede <edouard.pignede@student.ecp.fr>
2021-05-03 17:46:07 +0200

    Création dezs pngs de composition des indices en trois catégories

--- scripts/Edouard/Composition_index/Composition_investigation.R
+++ scripts/Edouard/Composition_index/Composition_investigation.R
@@ -28,23 +28,74 @@ for (Ville_cons in c("Marseille", "Bordeaux", "La Rochelle", "Nantes", "Rennes",
     
     Index_composition <- read.csv2("./scripts/Edouard/Composition_index_results.csv", row.names = NULL)
     
+    dir.create(paste0("./scripts/Edouard/Composition_index/Figures/", Ville_cons, "_", Type))
+    
     Index_composition <- Index_composition %>%
       filter(Ville == Ville_cons,
              Exports_imports == Type) %>%
-      pivot_wider(id_cols = "year", names_from = "Product_sector", values_from = c("Index_value", "Part_value", "Part_flux"))
+      pivot_wider(id_cols = "year", names_from = "Product_sector", values_from = c("Index_value", "Part_value", "Part_flux")) %>%
+      mutate(Part_value_Manufactures_percent = Part_value_Manufactures / Part_value_All,
+             `Part_value_Non-agricultural primary goods_percent` = `Part_value_Non-agricultural primary goods` / Part_value_All,
+             Part_value_Agriculture_percent = Part_value_Agriculture / Part_value_All)
+    
+    
+    png(paste0("./scripts/Edouard/Composition_index/Figures/", Ville_cons, "_", Type, "/Index_value.png"),      
+        width = 5000,
+        height = 2700,
+        res = 500)
+    
+    plot(drop_na(Index_composition[, c("year", "Index_value_All")]), type = "o", col = "black", ylim = c(50, 200), 
+         main = paste(Ville_cons, Type))
+    lines(drop_na(Index_composition[, c("year", "Index_value_Manufactures")]), type = "o", col = "red")
+    lines(drop_na(Index_composition[, c("year", "Index_value_Non-agricultural primary goods")]), type = "o", col = "green")
+    lines(drop_na(Index_composition[, c("year", "Index_value_Agriculture")]), type = "o", col = "blue")
+    legend("topleft", c("All", "Manufactures", "Non-agricultural primary goods", "Agriculture"),
+           col = c("black", "red", "green", "blue"), lty = 1)
+    
+    dev.off()
+    
+    png(paste0("./scripts/Edouard/Composition_index/Figures/", Ville_cons, "_", Type, "/Composition_volume.png"),      
+        width = 5000,
+        height = 2700,
+        res = 500)
+    
+    barplot(t(as.matrix(Index_composition[, c("Part_value_Manufactures", 
+                                              "Part_value_Non-agricultural primary goods", 
+                                              "Part_value_Agriculture")])),
+            names.arg = Index_composition$year,
+            legend.text = c("Manufactures", "Non-agricultural primary goods", "Agriculture"),
+            args.legend = list(x = 40, y = 1.1), 
+            main = paste(Ville_cons, Type))
+    
+    dev.off()
+    
+    png(paste0("./scripts/Edouard/Composition_index/Figures/", Ville_cons, "_", Type, "/Composition_part.png"),      
+        width = 5000,
+        height = 2700,
+        res = 500)
+    
+    barplot(t(as.matrix(Index_composition[, c("Part_value_Manufactures_percent", 
+                                              "Part_value_Non-agricultural primary goods_percent", 
+                                              "Part_value_Agriculture_percent")])),
+            names.arg = Index_composition$year,
+            legend.text = c("Manufactures", "Non-agricultural primary goods", "Agriculture"),
+            args.legend = list(x = "topright"), 
+            main = paste(Ville_cons, Type))
+    
+    dev.off()
     
     
     print(paste(Ville_cons, Type))
-    reg_compo <- lm(log(All) ~ log(Manufactures) + log(Agriculture) + log(`Non-agricultural primary goods`), 
-                    data = Index_composition)
-    
-    print(summary(reg_compo))
-    
-    
-    
-    Index_composition_mod <- Index_composition %>% rowwise() %>%
-      mutate(Index_recomp = weighted.mean(c(Index_value_Manufactures, Index_value_Agriculture, `Index_value_Non-agricultural primary goods`), 
-                                          c(Part_value_Manufactures, Part_value_Agriculture, `Part_value_Non-agricultural primary goods`)))
-    
+    # reg_compo <- lm(log(All) ~ log(Manufactures) + log(Agriculture) + log(`Non-agricultural primary goods`), 
+    #                 data = Index_composition)
+    # 
+    # print(summary(reg_compo))
+    # 
+    # 
+    # 
+    # Index_composition_mod <- Index_composition %>% rowwise() %>%
+    #   mutate(Index_recomp = weighted.mean(c(Index_value_Manufactures, Index_value_Agriculture, `Index_value_Non-agricultural primary goods`), 
+    #                                       c(Part_value_Manufactures, Part_value_Agriculture, `Part_value_Non-agricultural primary goods`)))
+    # 
   }
 }
